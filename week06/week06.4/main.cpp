0f7d945c1b201ec00b7fb8a944bd4e2db909e0b8
yu0986 <79677216+yu0986@users.noreply.github.com>
2021-03-31 15:43:20 +0800

    Rename week06.4/main.cpp to week06/week06.4/main.cpp

--- week06/week06.4/main.cpp
+++ week06/week06.4/main.cpp
@@ -2,20 +2,20 @@
 float angle=0; ///TODO:
 void hand(){
      glPushMatrix();
-        glScalef(0.5,0.1,0.1);///²Óªøªº
-        glColor3f(0,0,1);///ÂÅ¦âªº
-        glutSolidCube(1);///¤è¶ô
+        glScalef(0.5,0.1,0.1);///ç´°é•·çš„
+        glColor3f(0,0,1);///è—è‰²çš„
+        glutSolidCube(1);///æ–¹å¡Š
     glPopMatrix();
 }
 void display()
 {
-    glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT); ///²MªÅ
-    glColor3f(1,1,1); glutSolidCube(1);///¤j¨­Åé
+    glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT); ///æ¸…ç©º
+    glColor3f(1,1,1); glutSolidCube(1);///å¤§èº«é«”
     glPushMatrix();
-        glTranslatef(0.5,0.5,0);///(3)¾ã­Ó²¾¨ì¥k¤W¨¤±¾µÛ
-        glRotatef(angle, 0, 0, 1);///¹ï¤¤¤ßÂà°Ê
-        glTranslatef(0.25,0,0);///§âÃö¸`²¾¨ì¤¤¤ß
-        hand();///²ÓªøÂÅ¦â¤p¤â
+        glTranslatef(0.5,0.5,0);///(3)æ•´å€‹ç§»åˆ°å³ä¸Šè§’æ›è‘—
+        glRotatef(angle, 0, 0, 1);///å°ä¸­å¿ƒè½‰å‹•
+        glTranslatef(0.25,0,0);///æŠŠé—œç¯€ç§»åˆ°ä¸­å¿ƒ
+        hand();///ç´°é•·è—è‰²å°æ‰‹
        glPopMatrix();
     glutSwapBuffers();
     angle++; ///todo:
